bb32528f99e833a82e6dd3dbf38e849a92df974c
Andreas Linz <klingt.net@gmail.com>
2015-11-16 19:34:38 +0100

    Add benchmarks
    Currently there is only a benchmark for different oscillators.

---
+++ benches/osc.rs
@@ -0,0 +1,39 @@
+ #![feature(test)]
+extern crate test;
+use test::Bencher;
+
+extern crate ytterbium;
+use ytterbium::osc::{Osc,Simple,PhaseAccu,RotMat};
+
+const FS: usize = 48000;
+const F: f64 = 440.0;
+
+#[bench]
+fn bench_simple(b: &mut Bencher) {
+    let mut osc = Simple::new(FS, F, 0.0);
+    b.iter(|| {
+        for _ in 0..FS {
+            osc.tick();
+        }
+    });
+}
+
+#[bench]
+fn bench_phase_accu(b: &mut Bencher) {
+    let mut osc = PhaseAccu::new(FS, F, 0.0);
+    b.iter(|| {
+        for _ in 0..FS {
+            osc.tick();
+        }
+    });
+}
+
+#[bench]
+fn bench_rot_mat(b: &mut Bencher) {
+    let mut osc = RotMat::new(FS, F, 0.0);
+    b.iter(|| {
+        for _ in 0..FS {
+            osc.tick();
+        }
+    });
+}
