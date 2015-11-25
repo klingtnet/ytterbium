8e0dbbbc729669cfad18d573930af2bd03899c38
Andreas Linz <klingt.net@gmail.com>
2015-11-25 23:55:26 +0100

    Create triangle and square table, plot square

--- octave/osc.m
+++ octave/osc.m
@@ -15,7 +15,11 @@ phasor_wave = phasor(f,fs,phi,dur); phasor_err = abs(sum(phasor_wave - naive_wav
 sine_table = create_sine_table(ts,phi);
 itlo_wave  = itlo(f,fs,phi,dur,sine_table); itlo_err = abs(sum(itlo_wave - naive_wave));
 saw_table = create_saw_table(ts,phi,order);
+tri_table = create_triangle_table(ts,phi,order);
+sqr_table = create_square_table(ts,phi,order);
 itlo_saw_wave = itlo(f,fs,phi,dur,saw_table);
+itlo_tri_wave = itlo(f,fs,phi,dur,tri_table);
+itlo_sqr_wave = itlo(f,fs,phi,dur,sqr_table);
 
 subplot(2,3,1);
 spectrum_plot(fs, rot_wave, 'rot');
@@ -30,9 +34,12 @@ subplot(2,3,4);
 spectrum_plot(fs, itlo_wave, 'itlo');
 
 subplot(2,3,5);
-plot(itlo_saw_wave);
+#plot(itlo_saw_wave);
+plot(itlo_sqr_wave);
+
 subplot(2,3,6);
-spectrum_plot(fs, itlo_saw_wave, 'itlo_saw');
+#spectrum_plot(fs, itlo_saw_wave, 'itlo_saw');
+spectrum_plot(fs, itlo_sqr_wave, 'itlo_sqr');
 
 disp("Rotation Matrix error: ");disp(rot_err);
 disp("Phasor error: ");disp(phasor_err);
