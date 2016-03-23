78a08b235ca8f55f191286bb75c7337a5a6e4fb9
Andreas Linz <klingt.net@gmail.com>
2016-03-23 11:56:35 +0100

    Use ring-buffer between DSP part and audio output

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,13 @@ extern crate rosc;
 extern crate docopt;
 extern crate rustc_serialize;
 extern crate rsoundio;
+extern crate rb;
 
+use std::f32::consts::PI as PI32;
 use std::thread;
 use std::sync::mpsc; // multiple producer/single consumer
+
+use rb::{RB, RbProducer, RbConsumer};
 
 mod errors;
 use errors::RunError;
@@ -62,6 +66,8 @@ fn main() {
 }
 
 fn run(args: Args) -> Result<(), RunError> {
+    let buf = rb::SpscRb::new(4096);
+    let (producer, consumer) = (buf.producer(), buf.consumer());
     let (tx_receiver, rx_router) = mpsc::channel();
     let (tx_router, rx_dsp) = mpsc::channel();
     let mut osc_receiver = try!(OscReceiver::new(args.flag_addr,
@@ -85,26 +91,46 @@ fn run(args: Args) -> Result<(), RunError> {
     let _ = thread::Builder::new()
                 .name("dsp".to_owned())
                 .spawn(move || {
+                    let w = (2.0 * PI32 * 440.0) / 48_000.0;
+                    let mut n = 0;
+
                     loop {
                         // TODO: busy wait loop, should be not so bad when the actual dsp
                         // calculations take place
 
                         // TODO: dsp and audio output are going to need shared audio buffer
                         if let Ok(msg) = rx_dsp.try_recv() {
-                            // here comes the dsp code
+
                         }
+                        let data = (0..512)
+                                       .map(|x| {
+                                           n = (n + 1) % 110;
+                                           (w * n as f32).sin() * 0.5
+                                       })
+                                       .collect::<Vec<f32>>();
+                        let cnt = producer.write_blocking(&data).unwrap();
                     }
                 })
                 .unwrap();
 
-    let sio = rsoundio::SoundIo::new();
     // connect to default backend
-    sio.connect();
     sio.flush_events();
     let dev = sio.default_output_device().unwrap();
     let mut out = dev.create_outstream().unwrap();
-    // TODO: implement audio output in main thread
-
+    out.register_write_callback(|out: rsoundio::OutStream,
+                                 min_frame_count: u32,
+                                 max_frame_count: u32| {
+        // TODO: pulseaudio has problems with buffer sizes smaller than 2048
+        let mut data = vec![0.0f32; 2048];
+        let cnt = consumer.read_blocking(&mut data).unwrap();
+        let frames = vec![data.clone(), data.clone()];
+        out.write_stream_f32(min_frame_count, &frames).unwrap();
+    });
+    out.register_underflow_callback(|out: rsoundio::OutStream| {
+         println!("Underflow in {} occured!", out.name().unwrap())
+    });
+    out.open().unwrap();
+    out.start().unwrap();
     osc.join();
     Ok(())
 }
