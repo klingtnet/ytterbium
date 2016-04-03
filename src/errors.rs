812470529e0b9927f451b279c0355e63c72f1f13
Andreas Linz <klingt.net@gmail.com>
2016-04-03 18:03:22 +0200

    Remove unused error variant

--- src/errors.rs
+++ src/errors.rs
@@ -2,12 +2,10 @@ extern crate rosc;
 extern crate portmidi as midi;
 extern crate rsoundio;
 
-use std::net::AddrParseError;
 use std::io;
 
 #[derive(Debug)]
 pub enum RunError {
-    AddrError(AddrParseError),
     IoError(io::Error),
     OscError(rosc::OscError),
     MidiError(midi::Error),
