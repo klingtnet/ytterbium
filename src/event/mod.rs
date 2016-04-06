2db6fd812b8d5d77d609bdbfea30ca31d696f556
Andreas Linz <klingt.net@gmail.com>
2016-04-06 22:10:31 +0200

    Remove unused `translate_osc` function
    This must be implemented in `src/io/osc.rs`.

--- src/event/mod.rs
+++ src/event/mod.rs
@@ -1,8 +1,6 @@
 extern crate rosc;
 extern crate portmidi;
 
-use rosc::OscPacket;
-
 #[derive(Debug)]
 pub enum ControlEvent {
     Unknown,
@@ -16,26 +14,4 @@ pub enum ControlEvent {
         key: u8,
         velocity: f32,
     },
-}
-
-pub fn translate_osc(packet: rosc::OscPacket) -> ControlEvent {
-    // TODO: map OSC packet to a ControlEvent
-    match packet {
-        OscPacket::Message(msg) => {
-            println!("{:?}", msg);
-            let addr: Vec<&str> = msg.addr.split('/').filter(|part| !part.is_empty()).collect();
-            // TODO: differentiate how to handle args by means of address
-            // this means, call a different match based on the control address
-            match msg.args {
-                Some(args) => {
-                    // TODO: iterate over args and build message
-                    match args[0] {
-                        _ => ControlEvent::Unknown,
-                    }
-                }
-                None => ControlEvent::NoEvent,
-            }
-        }
-        OscPacket::Bundle(_) => ControlEvent::Unknown,
-    }
 }
