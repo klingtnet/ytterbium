9f820081bc6ed57e46f113aba2c0f1120a007d6e
Andreas Linz <klingt.net@gmail.com>
2016-05-07 12:12:43 +0200

    Add signal flow traits

--- src/dsp/mod.rs
+++ src/dsp/mod.rs
@@ -1,3 +1,5 @@
+use types::Stereo;
+
 mod env_gen;
 mod wavetable;
 mod voice;
@@ -5,3 +7,16 @@ mod voice;
 pub use self::env_gen::{ADSR, ADSRState};
 pub use self::wavetable::{Wavetable, WavetableOsc, generate_wavetables, Waveform};
 pub use self::voice::{VoiceManager};
+
+pub trait SignalSource {
+    fn tick(&mut self) -> Stereo;
+}
+pub trait SignalLink {
+    fn tick(&mut self, Stereo) -> Stereo;
+}
+pub trait SignalSink {
+    fn tick(&mut self, Stereo);
+}
+pub trait SignalFlow {
+    fn tick(&mut self);
+}
