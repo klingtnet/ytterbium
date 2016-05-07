8a67834412af489ec479866fb4f63d44b33174a3
Andreas Linz <klingt.net@gmail.com>
2016-05-07 12:18:03 +0200

    Add `dynamics` module
    This module contains a hard and soft volume limiters. The `HardLimiter`
    simply forces the input signal into the [-1,1] range by cutting of
    larger amplitudes. The `SoftLimiter` however, multiplies the input
    signal with an approximated tangents hyperbolicus (tanh) to force the
    signal into the [-1,1] range.

---
+++ src/dsp/dynamics.rs
@@ -0,0 +1,37 @@
+use types::Stereo;
+use dsp::SignalLink;
+
+pub struct HardLimiter {}
+impl SignalLink for HardLimiter {
+    fn tick(&mut self, input: Stereo) -> Stereo {
+        let mut output = input;
+        if input.0.abs() > 1.0 {
+            output.0 = 1.0 * input.1.signum();
+        }
+        if input.1.abs() > 1.0 {
+            output.1 = 1.0 * input.1.signum();
+        }
+        output
+    }
+}
+pub struct SoftLimiter {}
+impl SignalLink for SoftLimiter {
+    fn tick(&mut self, input: Stereo) -> Stereo {
+        // Padé approximation for tanh
+        // http://www.musicdsp.org/showone.php?id=238
+        match (input.0.abs() > 3.0, input.1.abs() > 3.0) {
+            (false, false) => input * (input * input + 27.0) / (input * input * 9.0 + 27.0),
+            (true, false) => {
+                Stereo(input.0.signum(), input.1)
+            }
+            (false, true) => {
+                Stereo(input.0, input.1.signum())
+            }
+            _ => {
+                Stereo(input.0.signum(), input.1.signum())
+            }
+        }
+    }
+}
+
+
--- src/dsp/mod.rs
+++ src/dsp/mod.rs
@@ -4,11 +4,13 @@ mod env_gen;
 mod wavetable;
 mod voice;
 mod flow;
+mod dynamics;
 
 pub use self::env_gen::{ADSR, ADSRState};
 pub use self::wavetable::{Wavetable, WavetableOsc, generate_wavetables, Waveform};
 pub use self::voice::{VoiceManager};
 pub use self::flow::{Flow, BufferSink};
+pub use self::dynamics::{HardLimiter, SoftLimiter};
 
 pub trait SignalSource {
     fn tick(&mut self) -> Stereo;
