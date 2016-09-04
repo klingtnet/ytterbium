00c89eb474d7ec0c92dee045a509374c2f34f0d9
Andreas Linz <klingt.net@gmail.com>
2016-09-04 22:51:33 +0200

    Implement a multimode filter
    This commit adds highpass, bandpass and notch filter characterics.

--- src/dsp/filter.rs
+++ src/dsp/filter.rs
@@ -20,10 +20,10 @@ pub struct Filter {
     Ys: [Stereo; 2],
 }
 impl Filter {
-    fn new(sample_rate: usize, fc: Float) -> Self {
+    fn new(sample_rate: usize, fc: Float, filter_type: &FilterType) -> Self {
         let w = 2.0 * PI * fc / sample_rate as Float;
         let q = 1.0;
-        let (As, Bs) = Filter::coeffs(w, q, FilterType::LP);
+        let (As, Bs) = Filter::coeffs(w, q, filter_type);
         Filter {
             sample_rate: sample_rate,
             fc: fc,
@@ -36,28 +36,52 @@ impl Filter {
         }
     }
 
-    fn coeffs(w: Float, q: Float, filter_type: FilterType) -> ([Float; 2], [Float; 3]) {
+    fn coeffs(w: Float, q: Float, filter_type: &FilterType) -> ([Float; 2], [Float; 3]) {
         let (sinw, cosw) = (Float::sin(w), Float::cos(w));
         let (mut As, mut Bs) = ([0.; 2], [0.; 3]);
         let alpha = sinw / (2.0 * q);
 
         let a0 = 1. + alpha;
-        // normalize by dividing through a0
-        match filter_type {
+        As[0] = -2. * cosw;
+        As[1] = 1. - alpha;
+        // only zeros differ
+        match *filter_type {
             FilterType::LP => {
-                As[0] = -2. * cosw / a0;
-                As[1] = (1. - alpha) / a0;
-                Bs[0] = (1. - cosw) * 0.5 / a0;
-                Bs[1] = (1. - cosw) / a0;
-                Bs[2] = (1. - cosw) * 0.5 / a0;
+                Bs[0] = (1. - cosw) / 2.;
+                Bs[1] = 1. - cosw;
+                Bs[2] = (1. - cosw) / 2.;
             }
-            _ => unimplemented!(),
+            FilterType::HP => {
+                Bs[0] = (1. + cosw) / 2.;
+                Bs[1] = -1. - cosw;
+                Bs[2] = (1. + cosw) / 2.;
+            }
+            FilterType::BP => {
+                Bs[0] = alpha;
+                Bs[1] = 0.;
+                Bs[2] = -alpha;
+            }
+            FilterType::Notch => {
+                Bs[0] = 1.;
+                Bs[1] = -2. * cosw;
+                Bs[2] = 1.;
+            }
+        }
+        // normalize by dividing through a0
+        for x in Bs.iter_mut().chain(As.iter_mut()) {
+            *x /= a0;
         }
         (As, Bs)
     }
 
-    fn set_freq(&mut self, freq: Float) {
-        self.coeffs = Filter::coeffs(2.0 * PI * freq / self.sample_rate as Float, self.q, FilterType::LP)
+    fn set_freq(&mut self, freq: Float, filter_type: &FilterType) {
+        self.coeffs = Filter::coeffs(2.0 * PI * freq / self.sample_rate as Float,
+                                     self.q,
+                                     filter_type)
+    }
+
+    fn set_q(&mut self, q: Float) {
+        unimplemented!()
     }
 }
 
@@ -79,17 +103,14 @@ mod tests {
     extern crate hound;
     extern crate rand;
 
-    use super::{Filter,FilterType};
+    use super::{Filter, FilterType};
     use super::super::SignalLink;
-    use types::{Float,Stereo,MINUS_THREE_DB};
+    use types::{Float, Stereo, MINUS_THREE_DB};
     use self::rand::distributions::{IndependentSample, Range};
 
     #[test]
     fn test_filter() {
         const SAMPLE_RATE: usize = 48_000;
-        let LOW_FREQ = 100.0;
-        let mut filter = Filter::new(SAMPLE_RATE, LOW_FREQ);
-        let filter_type = FilterType::LP;
         let num_samples = SAMPLE_RATE * 10;
 
         let wave_spec = hound::WavSpec {
@@ -99,23 +120,38 @@ mod tests {
             bits_per_sample: 32,
         };
         let scale = ::std::i32::MAX as Float;
-        let filename = format!("ytterbium-{}-{:?}-filter.wav", env!("CARGO_PKG_VERSION"), filter_type);
-        let mut writer = hound::WavWriter::create(filename, wave_spec).unwrap();
+        for filter_type in &[FilterType::LP, FilterType::HP, FilterType::BP, FilterType::Notch] {
+            let filename = format!("ytterbium-{}-{:?}-filter.wav",
+                                   env!("CARGO_PKG_VERSION"),
+                                   filter_type);
+            let mut writer = hound::WavWriter::create(filename, wave_spec).unwrap();
 
-        let range = Range::new(-MINUS_THREE_DB, MINUS_THREE_DB);
-        let mut rng = rand::thread_rng();
+            const MINUS_SIX_DB: Float = MINUS_THREE_DB * MINUS_THREE_DB;
+            let range = Range::new(-MINUS_SIX_DB, MINUS_SIX_DB);
+            let mut rng = rand::thread_rng();
 
-        let multiplier = 1.0 + ((20_000.0 as Float).ln() - (LOW_FREQ).ln()) / num_samples as Float;
-        let mut freq = LOW_FREQ;
+            let (start_freq, end_freq) = (100.0, 18_000.0);
+            let mut freq = match *filter_type {
+                FilterType::HP => end_freq,
+                _ => start_freq,
+            };
+            let mut filter = Filter::new(SAMPLE_RATE, freq, filter_type);
 
-        for idx in 0..num_samples {
-            let r = range.ind_sample(&mut rng);
-            let out = filter.tick(Stereo(r, r));
-            writer.write_sample((out.0 * scale) as i32).unwrap();
-            writer.write_sample((out.1 * scale) as i32).unwrap();
-            freq *= multiplier;
-            filter.set_freq(freq);
+            let multiplier = 1.0 +
+                             ((end_freq as Float).ln() - (start_freq).ln()) / num_samples as Float;
+
+            for idx in 0..num_samples {
+                let r = range.ind_sample(&mut rng);
+                let out = filter.tick(Stereo(r, r));
+                writer.write_sample((out.0 * scale) as i32).unwrap();
+                writer.write_sample((out.1 * scale) as i32).unwrap();
+                freq = match *filter_type {
+                    FilterType::LP | FilterType::BP | FilterType::Notch => freq * multiplier,
+                    FilterType::HP => freq / multiplier,
+                };
+                filter.set_freq(freq, filter_type);
+            }
+            writer.finalize().unwrap();
         }
-        writer.finalize().unwrap();
     }
 }
